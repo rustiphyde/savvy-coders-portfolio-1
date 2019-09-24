8581e4bf3b96c358155f9cf866acca78c2f1de5e
manavm1990 <manavm@visionwebsoft.com>
2019-09-23 19:10:27 -0500

    Add commented out example of getter.

--- scripts.js
+++ scripts.js
@@ -10,6 +10,9 @@ function greeter(question = "What is your name?") {
   return answer;
 }
 
+// // We are GETTING the value of the textContent property - WE ARE NOT SETTING IT!
+// const getter = document.querySelector("h1").textContent;
+
 // TODO: Resolve issue where h1 is not updated if user gives blank answer
 document.querySelector("h1").textContent = greeter();
 
