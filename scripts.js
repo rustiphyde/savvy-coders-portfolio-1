c610ad9229730a06bf3afcb18559781ac84849f7
manavm1990 <manavm@visionwebsoft.com>
2019-09-24 20:54:29 -0500

    Add skeleton for hamburger click listener.

--- scripts.js
+++ scripts.js
@@ -1,9 +1,8 @@
-// Listen for a click on the hamburger
 // Remove the 'mobile hidden' class from the ul
 
 const hamburger = document.querySelector(".fa-hamburger");
 const hiddenUL = document.querySelector("ul");
 
 hamburger.addEventListener("click", function() {
-
+  console.log(hiddenUL.classList);
 });
