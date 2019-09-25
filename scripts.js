9825fac32cd34431686c8bc9638641ed3ae335de
manavm1990 <manavm@visionwebsoft.com>
2019-09-24 21:12:58 -0500

    Add JS to toggle the nav menu.

--- scripts.js
+++ scripts.js
@@ -4,5 +4,6 @@ const hamburger = document.querySelector(".fa-hamburger");
 const hiddenUL = document.querySelector("ul");
 
 hamburger.addEventListener("click", function() {
-  console.log(hiddenUL.classList);
+  // hiddenUL.classList.remove("is-hidden--mobile");
+  hiddenUL.classList.toggle("is-hidden--mobile");
 });
