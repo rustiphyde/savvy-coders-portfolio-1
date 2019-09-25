d6423c66d52cddf44e18bc96a12528f1b13e75f6
manavm1990 <manavm@visionwebsoft.com>
2019-09-24 20:48:23 -0500

    Select the hamburger and hidden ul.

--- scripts.js
+++ scripts.js
@@ -1,26 +1,6 @@
-// Write a fxn. to where I can pass in a string that will end up being in the window prompt
+// Listen for a click on the hamburger
+// Remove the 'mobile hidden' class from the ul
 
-function greeter(question = "What is your name?") {
-  let answer = prompt(question);
+const hamburger = document.querySelector(".fa-hamburger");
+const hiddenUL = document.querySelector("ul");
 
-  if (answer === "") {
-    return greeter(); // recursion
-  }
-
-  return answer;
-}
-
-// // We are GETTING the value of the textContent property - WE ARE NOT SETTING IT!
-// const getter = document.querySelector("h1").textContent;
-
-// document.querySelector("#root").innerHTML = "<p>" + greeter() + "</p>";
-
-document.querySelector("#root").innerHTML = `<p>${greeter()}</p>`
-// document.querySelector("#root").textContent = "<p>Hello World!</p>";
-
-// const answer = greeter();
-// document.querySelector("h1").textContent = answer
-
-// const h1 = document.querySelector("h1");
-// const textContent = h1.textContent;
-// const h1TextContent = document.querySelector("h1").textContent;
