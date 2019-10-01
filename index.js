a88ff8a1107f063e55ef75b40a8e177d4bacc61f
manavm1990 <manavm@visionwebsoft.com>
2019-09-30 20:03:08 -0500

    Add state to page.

--- index.js
+++ index.js
@@ -2,12 +2,25 @@
 import { Header, Nav, Main, Footer } from "./components";
 
 /**
+ * Use `state` to render the appropriate heading depending on the state of the app.
+ * What page is currently selected or being displayed.
+ */
+const state = {
+  home: {
+    heading: "Home Page"
+  },
+  about: {
+    heading: "About Page"
+  }
+};
+
+/**
  * Currently, #root div is empty.
  * We want to grab that #root div.
  * We want to assign the markup that is contained in the components as the innerHTML of root.
  */
 document.querySelector("#root").innerHTML = `
-  ${Header()}
+  ${Header(state.home.heading)}
   ${Nav()}
   ${Main()}
   ${Footer()}
